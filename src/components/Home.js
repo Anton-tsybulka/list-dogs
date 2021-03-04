6d21fcf3134dd52aca22de9e5a968d01326b892b
Anton Tsybulka <anton.kurto@gmail.com>
2021-03-04 15:06:23 +0300

    add button

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react';
-import { Carousel } from 'antd';
+import { Carousel, Button } from 'antd';
 import { useDispatch, useSelector } from 'react-redux';
 import { getAllBreeds } from '../redux/actions/allBreedsActions';
 
@@ -12,18 +12,33 @@ const Home = () => {
         dispatch(getAllBreeds());
     }, [dispatch]);
 
+    const onClick = () => {
+        dispatch(getAllBreeds());
+    };
+
     const imagesForCarousel = allBreeds.map((item, id) => {
         return (
             <div key={id}>
-                <img src={item} alt='dog' style={{margin: 'auto', height: '400px'}}/>
+                <img src={item} alt='dog' style={{margin: 'auto', height: '580px'}}/>
             </div>
         )
     });
 
     return (
-        <Carousel autoplay>
-            {imagesForCarousel}
-        </Carousel>
+        <>
+            <h2 style={{textAlign: 'center', marginTop: '5em'}}>
+                If you want to see other photos, then click the button
+            </h2>
+            <Button 
+                type="primary"
+                onClick={() => onClick()} 
+                style={{margin: '1em 47% 5em'}}>
+                Click me
+            </Button>            
+            <Carousel autoplay>
+                {imagesForCarousel}
+            </Carousel>
+        </>
     );
 };
 
